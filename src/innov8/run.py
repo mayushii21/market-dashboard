77136cfbb4ff5a08fcf361d1aae06ea13e5f5dee
mayushii21 <122178787+mayushii21@users.noreply.github.com>
2024-07-07 18:29:51 -0500

    add env conditional

--- src/innov8/run.py
+++ src/innov8/run.py
@@ -1,11 +1,19 @@
+import os
+
+from dotenv import load_dotenv
+
 from innov8.layout import app
 
+load_dotenv(".safe_env")
 # Create server for gunicorn
 server = app.server
 
 
-def main():
-    app.run(debug=False)
+def main() -> None:
+    if os.getenv("DEV_ENV"):
+        app.run(debug=True, threaded=True)
+    else:
+        app.run_server(debug=False, threaded=True)
 
 
 if __name__ == "__main__":
