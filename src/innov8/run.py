67bf424d6a65bc90d1f12fcb3ab7d76e44afab81
mayushii21 <122178787+mayushii21@users.noreply.github.com>
2024-07-08 08:44:39 -0500

    create cli function for parsing cli arguments

--- src/innov8/run.py
+++ src/innov8/run.py
@@ -1,14 +1,36 @@
+import argparse
 import os
 import sys
 
 from dotenv import load_dotenv
 from loguru import logger
 
+from innov8 import update_all
 from innov8.layout import app
 
 load_dotenv(".safe_env")
 # Create server for gunicorn
 server = app.server
+
+
+def cli() -> None:
+    parser = argparse.ArgumentParser(description="Innov8 Application CLI")
+    parser.add_argument(
+        "command",
+        nargs="?",
+        default="run",
+        choices=["run", "update"],
+        help='Run the main application (default) or update all tickers with "update"',
+    )
+
+    args = parser.parse_args()
+
+    if args.command == "update":
+        update_all.main()
+    elif args.command == "run":
+        main()
+    else:
+        parser.print_help()
 
 
 def main() -> None:
