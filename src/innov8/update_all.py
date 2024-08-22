28192a5de67d0e91395a2dba623c91370dca4652
mayushii21 <122178787+mayushii21@users.noreply.github.com>
2024-08-21 21:59:01 -0500

    generate forecasts after updating

--- src/innov8/update_all.py
+++ src/innov8/update_all.py
@@ -5,11 +5,17 @@ from innov8.db_ops import data
 
 
 def main() -> None:
+    symbols = data.main_table.symbol.unique()
     logger.info("Updating all...")
-    for symbol in tqdm(data.main_table.symbol.unique()):
+    for symbol in tqdm(symbols):
         data.add_new_ohlc(symbol)
     # Create an empty signal file to notify the main process of the update
     open(data.script_directory / "update_signal", "w").close()
+
+    logger.info("Training models and generating forecasts...")
+    data.clear_forecasts()
+    for symbol in tqdm(symbols):
+        data.generate_forecast(symbol)
 
 
 if __name__ == "__main__":
